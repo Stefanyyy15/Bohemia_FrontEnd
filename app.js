a98a979af216420affbddc46884fc7349a678978
Stefanyyy15 <yuli.estefani.s15@gmail.com>
2025-01-30 13:14:10 -0500

    feat: :sparkles: We add function Post

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const url1 = "http://localhost:8080/api/users";
 const peticionGet = async (url) => {
     try {
         const respuesta = await fetch(url, {
-            //credentials: 'include',
             headers: {
-                //'Accept': 'application/json',
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJjYW1wdXNjbCIsInN1YiI6IlBhekVuRWxBcmlwb3JvQGVtYWlsLmNvbSIsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE3MzgyNTM2OTIsImV4cCI6MTczOTExNzY5Mn0.NF7WvRmMlRBj5qJ5BciFg2nT_Hs02WhhyMLdjSX7euf9Vx9X_zV914fxWPkNuQJJO7qZ0_nYNzh7j3GmLVxmgw'
             }
@@ -25,9 +23,34 @@ const peticionGet = async (url) => {
     }
 }
 
-const mostrarDatos = async(url)=>{
+const mostrarDatos = async (url) => {
     const respuesta = await peticionGet(url);
     console.log(respuesta);
 }
 
 mostrarDatos(url1);
+
+async function peticionPost(url, data) {
+    try {
+        const respuesta = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJjYW1wdXNjbCIsInN1YiI6IlBhekVuRWxBcmlwb3JvQGVtYWlsLmNvbSIsImF1dGhvcml0aWVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE3MzgyNTM2OTIsImV4cCI6MTczOTExNzY5Mn0.NF7WvRmMlRBj5qJ5BciFg2nT_Hs02WhhyMLdjSX7euf9Vx9X_zV914fxWPkNuQJJO7qZ0_nYNzh7j3GmLVxmgw'
+            },
+            body: JSON.stringify(data)
+        });
+        console.log('Response Status', respuesta.status);
+        if (respuesta.ok) {
+            return await respuesta.json();
+        } else {
+            console.error('Error', respuesta.status);
+            const textoError = await respuesta.text();
+            console.error('Detalle del error:', textoError);
+            return null;
+        }
+    }catch(error){
+        console.error("Error POST", error);
+    return null;
+    }
+}
