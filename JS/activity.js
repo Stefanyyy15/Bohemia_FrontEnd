83d4b856ff7b56b2cdd4fb14f9b51161ac71a651
Stefanyyy15 <yuli.estefani.s15@gmail.com>
2025-02-06 12:08:32 -0500

    feat: :sparkles: We created the activity section

---
+++ JS/activity.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function () {
+    let preloader = document.getElementById("preloader");
+    preloader.classList.add("fade-out");
+    setTimeout(() => {
+        preloader.style.display = "none";
+        document.getElementById("contenido").classList.remove("hidden");
+    }, 1000);
+});
